4fae15b55ac21c9638837d4e9d8393ebde516b8a
Kerri Crawford <kerriannercrawford+github@gmail.com>
2021-11-10 19:41:42 -0500

    Consumer action working

--- test/consume.spec.js
+++ test/consume.spec.js
@@ -1,15 +1,51 @@
 /* eslint-disable no-unused-vars */
-const messages = require('../lib/messages');
-const processAction = require('../lib/triggers/consume').process;
+const td = require('testdouble');
+const { expect } = require('chai');
+
+const kafkaClient = {};
+kafkaClient.consumer = () => {
+	return {
+		connect: () => true,
+		subscribe: () => true,
+		disconnect: () => true,
+		run: () => true
+	};
+};
+
+const componentConfig = {
+	topic: 'test-topic',
+	'bootstrap-servers': 'test',
+	'sasl-username': 'test',
+	'sasl-password': 'test',
+	'security-protocol': 'sasl_plain',
+	'sasl-mechanisms': 'PLAIN',
+};
 
 describe('consume action', () => {
+	let process, kafka, emit, that;
 	beforeEach(() => {
+		that = {
+			emit: (data, msg) => msg,
+			logger: {
+				info: () => true
+			}
+		};
+
+		kafka = td.replace('../lib/kafka');
+		emit = td.function();
+		process = require('../lib/triggers/consume').process;
+	
+		td.when(kafka.createKafka(td.matchers.anything())).thenReturn(kafkaClient);
+		td.when(emit('data', { topic: 'test-topic', partition: 0, key: 'hello', value: 'world' }));
 	});
 
 	afterEach(() => {
+		td.reset();
 	});
 
 	it('consume message', async () => {
+		await process.call(that, {}, componentConfig, {});
+		td.verify(emit('data', { topic: 'test-topic', partition: 0, key: 'hello', value: 'world' }));
 	});
 
 	it('reconnect on error', async () => {
